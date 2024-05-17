a9e889cce43e8c9cc18a0c58d7435eee0a51dd08
Tim Pepper <tjpepper93@gmail.com>
2024-05-16 21:28:23 -0400

    WIP: figure out how to get the wasm working...

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use std::time::Instant;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub enum Time {
+    Seconds,
+    Millis,
+    Micros,
+    Nanos
+}
+
+#[wasm_bindgen]
+pub struct Timer {
+    start: Instant
+}
+
+#[wasm_bindgen]
+impl Timer {
+    pub fn start() -> Timer {
+        Timer {
+            start: Instant::now()
+        }
+    }
+
+    pub fn end(&self, time: Time) -> String {
+        let end = Instant::now();
+        let duration = end.duration_since(self.start);
+        match time {
+            Time::Seconds => duration.as_secs().to_string(),
+            Time::Millis => duration.as_millis().to_string(),
+            Time::Micros => duration.as_micros().to_string(),
+            Time::Nanos => duration.as_nanos().to_string(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let timer = Timer::start();
+        std::thread::sleep(Duration::from_secs(1));
+        let end = timer.end(Time::Seconds);
+
+        assert_eq!(end, "1");
+    }
+}
